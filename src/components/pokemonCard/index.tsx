ee67f0fd04533aaac317abb7d7345872c81d50c7
camilla <camillaalves285@gmail.com>
2024-10-30 17:05:08 -0300

    feat: adicionei  o componente do card que vai renderizar os pokemons

---
+++ src/components/pokemonCard/index.tsx
@@ -0,0 +1,30 @@
+import Card from 'react-bootstrap/Card'
+import S from './styles.module.scss'
+
+type PokemonCardProps = {
+  src: string
+  name: string
+  type: string[]
+  id: number
+}
+
+const PokemonCard = ({ src, name, type, id }: PokemonCardProps) => {
+  return (
+    <Card className={S.card}>
+    <Card.Img variant="top" src={src} className={S.img} />
+    <Card.Text className={S.idPoke}>Nº {String(id).padStart(4, '0')}</Card.Text>
+    <Card.Body>
+      <Card.Title className={S.name}>{name}</Card.Title>
+      <Card.Text className={S.typesContainer}>
+        {type.map((t) => (
+          <div key={t} className={`${S.type} ${S[t]}`}>
+            {t}
+          </div>
+        ))}
+      </Card.Text>
+    </Card.Body>
+  </Card>
+  )
+}
+
+export default PokemonCard
