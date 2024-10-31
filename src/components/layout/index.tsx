f6b95b26835eb26509ae8951a4df3b4c3916e1a0
camilla <camillaalves285@gmail.com>
2024-10-31 15:17:17 -0300

    feat: importei  o componente do context e adicionei a função ao Header

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,13 +1,20 @@
 import { Outlet } from 'react-router-dom'
 import Header from '../header'
+import { useSearch } from '../context/SearchContext'
 
 const Layout = () => {
+  const { setSearchTerm } = useSearch(); // Pega a função para atualizar o termo de busca
+
+  const handleSearchChange = (value: string) => {
+    setSearchTerm(value); // Atualiza o estado no contexto
+  };
+
   return (
-    <div>
-      <Header />
+    <>
+      <Header onSearchChange={handleSearchChange} />
       <Outlet />
-    </div>
-  )
-}
+    </>
+  );
+};
 
 export default Layout
