80531aa3ce4053231fc60878bcf726da441de6af
camilla <camillaalves285@gmail.com>
2024-10-31 16:46:49 -0300

    feat:  adicionei o Loading a página home

--- src/components/spinnerError/index.tsx
+++ src/components/spinnerError/index.tsx
@@ -1,9 +1,9 @@
 import Spinner from 'react-bootstrap/Spinner';
+import S from './styles.module.scss';
 
 const SpinnerError = () => {
     return (
-        <Spinner animation="border" role="status">
-          <span className="visually-hidden">Loading...</span>
+        <Spinner animation="border" role="status" className={S.spinnerLoading}>
         </Spinner>
       );
 }
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,8 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+
 import { useEffect, useState } from 'react';
 import PokemonCard from '../../components/pokemonCard';
-import { Container } from 'react-bootstrap';
+import { Container } from 'react-bootstrap'; // Importando o Spinner
 import api from '../../services/api';
 import { useSearch } from '../../components/context/SearchContext';
 import S from './styles.module.scss';
@@ -18,35 +19,37 @@ const Home = () => {
   const [pokemonData, setPokemonData] = useState<PokemonDataItem[]>([]);
   const [filteredData, setFilteredData] = useState<PokemonDataItem[]>([]);
   const { searchTerm } = useSearch();
+  const [loading, setLoading] = useState(true); // Estado de carregamento
 
   useEffect(() => {
     const fetchPokemonData = async () => {
       try {
+        setLoading(true); // Inicia o carregamento
         const response = await api.get('/pokemon?limit=30&offset=0');
         const results = response.data.results;
 
         const pokemonDetails = await Promise.all(
-          results.map(async (item: { name: string }, index: number) => {
+          results.map(async (item: { name: string; }, index: number) => {
             const pokemonDetail = await api.get(`/pokemon/${item.name}`);
-            const types = Array.isArray(pokemonDetail.data.types)
-              ? pokemonDetail.data.types.map(
-                  (typeInfo: any) => typeInfo.type.name,
-                )
+            const types = Array.isArray(pokemonDetail.data.types) 
+              ? pokemonDetail.data.types.map((typeInfo: any) => typeInfo.type.name)
               : [];
 
             return {
               name: item.name.charAt(0).toUpperCase() + item.name.slice(1),
               src: `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${String(index + 1).padStart(3, '0')}.png`,
               type: types,
-              id: index + 1,
+              id: index + 1
             };
-          }),
+          })
         );
 
         setPokemonData(pokemonDetails);
         setFilteredData(pokemonDetails);
       } catch (error) {
-        console.error('Erro ao buscar dados do Pokémon:', error);
+        console.error("Erro ao buscar dados do Pokémon:", error);
+      } finally {
+        setLoading(false); // Termina o carregamento
       }
     };
 
@@ -57,21 +60,24 @@ const Home = () => {
     const filteredResults = pokemonData.filter(
       (pokemon) =>
         pokemon.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
-        pokemon.type.some((t) =>
-          t.toLowerCase().includes(searchTerm.toLowerCase()),
-        ),
+        pokemon.type.some(t => t.toLowerCase().includes(searchTerm.toLowerCase()))
     );
+
     setFilteredData(filteredResults);
   }, [searchTerm, pokemonData]);
 
   return (
     <Container className={S.container}>
-      {filteredData.length > 0 ? (
-        filteredData.map((item: PokemonDataItem) => (
-          <PokemonCard key={item.name} src={item.src} name={item.name} type={item.type} id={item.id} />
-        ))
+      {loading ? ( // Verifica se está carregando
+         <div className={S.Loading}> <SpinnerError/> </div>
       ) : (
-        <p className={S.notFoundMessage}> <SpinnerError/> Pokémon não encontrado</p> // Mensagem quando nenhum Pokémon é encontrado
+        filteredData.length > 0 ? (
+          filteredData.map((item: PokemonDataItem) => (
+            <PokemonCard key={item.name} src={item.src} name={item.name} type={item.type} id={item.id} />
+          ))
+        ) : (
+          <div className={S.notFoundMessage}> <SpinnerError/> Pokémon não encontrado</div> // Mensagem quando nenhum Pokémon é encontrado
+        )
       )}
     </Container>
   );
