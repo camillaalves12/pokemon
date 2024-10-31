8c32557f62fc15cfa9081c3471cba70bb3bdc67a
camilla <camillaalves285@gmail.com>
2024-10-31 15:16:48 -0300

    feat: importei  o componente do context

--- src/router.tsx
+++ src/router.tsx
@@ -1,20 +1,20 @@
 import { createBrowserRouter } from 'react-router-dom'
-import Contact from './pages/contact'
 import Layout from './components/layout'
 import Home from './pages/home'
+import { SearchProvider } from './components/context/SearchContext'
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <Layout />,
+    element: (
+      <SearchProvider>
+        <Layout />
+      </SearchProvider>
+    ),
     children: [
       {
         path: '/',
         element: <Home />,
-      },
-      {
-        path: '/contact',
-        element: <Contact />,
       },
     ],
   },
